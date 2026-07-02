1ba473214d57e107677eee117119feb588063901
erkumarshubham01-blip <er.kumarshubham01@gmail.com>
2026-07-02 21:44:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+const startButton = document.getElementById("startBtn");
 
+startButton.addEventListener("click", () => {
+
+    const selectedPaper = document.getElementById("paper").value;
+
+    alert("Selected Paper : " + selectedPaper);
+
+});
